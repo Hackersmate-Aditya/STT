f6d57f1eb98315785a3e72fc82543eda232f3d4f
Hackersmate-Aditya <68347060+Hackersmate-Aditya@users.noreply.github.com>
2022-04-28 11:34:09 +0530

    Add files via upload

---
+++ nioce_reduction.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
+    print('Speak anything')
+    audio = r.listen(source)
+
+    try:
+      text = r.recognize_google(audio)
+      print('you said:{}'.format(text))
+    except:
+        print('Sorry could not recognize speech')
+
